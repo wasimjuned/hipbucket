ebe719c61d039831073bb5a0fad0b64d11844245
inglesp <peter.inglesby@gmail.com>
2012-03-30 22:09:56 +0100

    HIPBUCKET initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipbucket::Application.config.secret_token = '588f62fce341000bc2c93bc5ad7573d84f5dcc857ad84e30745ab318902b9ef7f1072c5d2c0c2ed511810695db3383e27f6fec06e74458f8be56f83bcf77b2f1'
