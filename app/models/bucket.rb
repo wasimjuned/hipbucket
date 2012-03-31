fd3aab272815d779bc5ab15b8b75c18d531d4b26
inglesp <peter.inglesby@gmail.com>
2012-03-31 10:23:29 +0100

    Add callbacks and validations to Bucket model

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,2 +1,12 @@
 class Bucket < ActiveRecord::Base
+  # callbacks
+  before_create :generate_hashes
+
+  # validations
+  validates_presence_of :name, :description, :admin_email, :admin_hash, :participation_hash
+
+  def generate_hashes
+    self.admin_hash = SecureRandom.urlsafe_base64
+    self.participation_hash = SecureRandom.urlsafe_base64
+  end
 end
