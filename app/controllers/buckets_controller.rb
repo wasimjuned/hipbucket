baec0022e43af67166ef93e2dbd4dea46fb42bad
inglesp <peter.inglesby@gmail.com>
2012-03-31 11:37:24 +0100

    Flesh out buckets controller

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,12 +1,26 @@
 class BucketsController < ApplicationController
   def new
-    
+    @bucket = Bucket.new
   end
 
-  def edit
+  def create
+    @bucket = Bucket.new params[:bucket]
+    if @bucket.save
+      redirect_to @bucket
+    else
+      render :action => :new
+    end
+  end
+
+  def update
+    @bucket = Bucket.find_by_participation_hash params[:id]
+    if :success
+    else
+    end
   end
 
   def show
+    @bucket = Bucket.find_by_participation_hash params[:id]
+    @admin = (params[:key] == @bucket.admin_hash)
   end
-
 end
